0f6dea02239ba27c76394c0dad6b490d4ae06e55
stephenleader <1252527439@qq.com>
2022-07-11 11:19:52 +0800

    biangeng

---
+++ allinone/src/test/java/com/example/allinone/AllinoneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.allinone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AllinoneApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
